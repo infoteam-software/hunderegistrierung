47042dd33545d8543d8d5c53f9ce058e00df4396
Tamara Willacker <tamara.willacker@infoteam.de>
2021-06-03 11:04:26 +0200

    Added database preparation to Benchmarker

--- src/main/java/Benchmarker.java
+++ src/main/java/Benchmarker.java
@@ -2,6 +2,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import hunderegistrierung.legacy.register.DogRegister;
+import shared.DatabasePreparer;
 
 import java.util.Locale;
 
@@ -10,6 +11,7 @@ public class Benchmarker {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.ENGLISH);
         org.apache.log4j.BasicConfigurator.configure();
+        DatabasePreparer.setUpDatabase();
 
         Options opt = new OptionsBuilder()
                 .include(DogRegister.class.getSimpleName())
